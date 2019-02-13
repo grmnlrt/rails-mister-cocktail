2d37d953960d89caac5c23b5a3e884f7d9c6b865
Sophie Henry <sophiesophiehenry@googlemail.com>
2019-02-12 16:00:26 -0800

    added initialize on precompile

--- config/application.rb
+++ config/application.rb
@@ -16,10 +16,16 @@ require "sprockets/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+config.assets.initialize_on_precompile = false
+
+
 module RailsMisterCocktail
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+
+    config.assets.initialize_on_precompile = false
+
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
