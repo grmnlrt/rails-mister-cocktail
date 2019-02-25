f5fcd85aa75b5c899a8ec469713a0fc7e503cf0f
Sophie Henry <sophiesophiehenry@googlemail.com>
2019-02-25 10:41:49 -0800

    checking commits are al up to date and fixed positioning  between index and second page

--- config/application.rb
+++ config/application.rb
@@ -16,8 +16,6 @@ require "sprockets/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-config.assets.initialize_on_precompile = false
-
 
 module RailsMisterCocktail
   class Application < Rails::Application
